8daefd81b7c30d0141f95ffb57d2ff96111a24f3
dilrukr <dilrukr7@gmail.com>
2023-10-19 14:38:15 +0530

    commit1

--- src/Components/3Dmodels/Model.jsx
+++ src/Components/3Dmodels/Model.jsx
@@ -4,7 +4,7 @@ import { useFrame } from "@react-three/fiber";
 const actionNames = ["The Life"];
 
 const FantasyBook = () => {
-  const model = useGLTF("/public/fantasyBook/medieval_fantasy_book.glb");
+  const model = useGLTF("../../../public/fantasyBook/fantasyBookModded-v1.glb");
 
   const animations = useAnimations(model.animations, model.scene);
 
